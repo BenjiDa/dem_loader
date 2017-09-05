a76353ce15f7084eb092396ce6e9dba8cdbf7a57
Benjamin L Melosh <bmelosh@usgs.gov>
2017-09-05 12:57:44 -0700

    added dem download function, removed .ipynb files

--- download_dem.py
+++ download_dem.py
@@ -1,29 +1,52 @@
 
-import urllib2
+import urllib.request  # works with python3
 from bs4 import BeautifulSoup
 import os
-
-#User will input lat long and will have to round it to the correct DEM, so whatever position is inside the dem it will download that dem
-latitude = 40
-longitude = 124
-
-resoultion = 13 #for 1/3 arc second data
-kind = IMG #for .img files
-location = 'n%sw%s' % (str(latitude), str(longitude))
-
-URL = 'ftp://rockyftp.cr.usgs.gov/vdelivery/Datasets/Staged/NED/%s/%s/' % (str(resolution), str(kind))
+import math
+import wget
+import lxml
 
 
-page = urllib2.urlopen(URL)
-soup = BeautifulSoup(page, "lxml")
-div = str(soup.find('div', class_ = 'well'))
-log = open("log.txt", "w")
-log.write(div)
-log.close()
-f = open("log.txt")
-lines = f.readlines()
-line = lines[short_name_line]
-short_name = line[short_name_shear:]
-f.close()
-os.remove("log.txt")
-short_name = short_name.rstrip()
+# User will input lat long which will be rounded to the top left corner of the DEM.
+# A DEM will be downloaded that contains the lat,long inputs specified by
+# the user.
+
+def dem_loader(latitude, logitude, resolution=13, kind='IMG'):
+	'''
+	A function that downloads DEM data from the National Elevation Dataset. Currently it only works for 1/3 arcsecond data (~10m),
+	but this can be expanded upon.
+
+	User inputs:
+	latitude = latitude position of area of interest
+	longitude = longitude position of area of interest
+	resoultion = the data resolution (default 1/3 arcsecond resolution (~10 meter))
+	kind = the format (default is .IMG), this can be expanded upon in the future
+	'''
+
+    # Latitude and longitude are rounded up to position of the top
+    # left corner of the DEM
+    longitude = abs(longitude)  # Get absolute value of longitude
+    longitude = math.ceil(longitude) # Round up
+    latitude = math.ceil(latitude) # Round up
+
+
+    location = 'n%sw%s' % (str(latitude), str(longitude)) # Set the string that points to the DEM
+    name = 'USGS_NED_%s_%s_%s.zip' % (str(resolution), location, kind) # Set to full string
+    short_name = 'dem_%s' % location # Set name of new folder to put data into
+
+    URL = 'ftp://rockyftp.cr.usgs.gov/vdelivery/Datasets/Staged/NED/%s/%s/' % (str(resolution), kind) # Full URL to DEM
+
+    source_directory = os.getcwd() # Get current directory
+    data_directory = os.path.join(source_directory, short_name) # Set path name to new directory
+
+    # Check and see if the local directory currently exists or not, if not create that directory
+    if os.path.exists(data_directory):
+        os.chdir(data_directory)
+    else:
+        os.makedirs(short_name)
+        os.chdir(data_directory)
+
+    # Download data (This will take time)
+    url_name = URL+name
+    wget.download(url_name)
+
