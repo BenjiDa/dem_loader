9fb7ac35e2d11f203756e60fff468d7fa25d54a4
Benjamin L Melosh <bmelosh@usgs.gov>
2017-09-05 15:01:29 -0700

    test loader

--- download_dem.py
+++ download_dem.py
@@ -6,36 +6,35 @@ import math
 import wget
 import lxml
 
-
 # User will input lat long which will be rounded to the top left corner of the DEM.
 # A DEM will be downloaded that contains the lat,long inputs specified by
 # the user.
 
-def dem_loader(latitude, logitude, resolution=13, kind='IMG'):
-	'''
-	A function that downloads DEM data from the National Elevation Dataset. Currently it only works for 1/3 arcsecond data (~10m),
-	but this can be expanded upon.
+def dem_loader(latitude, longitude, resolution=13, kind='IMG'):
+    '''
+    A function that downloads DEM data from the National Elevation Dataset. Currently it only works for 1/3 arcsecond data (~10m),
+    but this can be expanded upon.
 
-	User inputs:
-	latitude = latitude position of area of interest
-	longitude = longitude position of area of interest
-	resoultion = the data resolution (default 1/3 arcsecond resolution (~10 meter))
-	kind = the format (default is .IMG), this can be expanded upon in the future
-	'''
+    User inputs:
+    latitude = latitude position of area of interest
+    longitude = longitude position of area of interest
+    resoultion = the data resolution (default 1/3 arcsecond resolution (~10 meter))
+    kind = the format (default is .IMG), this can be expanded upon in the future
+    '''
 
-    # Latitude and longitude are rounded up to position of the top
-    # left corner of the DEM
+    #Latitude and longitude are rounded up to position of the top
+    #left corner of the DEM
     longitude = abs(longitude)  # Get absolute value of longitude
     longitude = math.ceil(longitude) # Round up
     latitude = math.ceil(latitude) # Round up
-
+    
 
     location = 'n%sw%s' % (str(latitude), str(longitude)) # Set the string that points to the DEM
     name = 'USGS_NED_%s_%s_%s.zip' % (str(resolution), location, kind) # Set to full string
     short_name = 'dem_%s' % location # Set name of new folder to put data into
 
     URL = 'ftp://rockyftp.cr.usgs.gov/vdelivery/Datasets/Staged/NED/%s/%s/' % (str(resolution), kind) # Full URL to DEM
-
+    
     source_directory = os.getcwd() # Get current directory
     data_directory = os.path.join(source_directory, short_name) # Set path name to new directory
 
---
+++ download_dem_run.py
@@ -0,0 +1,6 @@
+from download_dem import dem_loader
+
+
+lat=35  #39
+lon=-104 #-122
+dem_loader(latitude=lat, longitude=lon)
