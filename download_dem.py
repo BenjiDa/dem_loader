1b51cac4186fc03e2d47fb63513981849450794c
Ben Melosh <benmelosh@gmail.com>
2017-09-05 09:45:22 -0700

    add git ignore

--- download_dem.py
+++ download_dem.py
@@ -1,10 +1,29 @@
 
+import urllib2
+from bs4 import BeautifulSoup
+import os
+
 #User will input lat long and will have to round it to the correct DEM, so whatever position is inside the dem it will download that dem
 latitude = 40
 longitude = 124
 
 resoultion = 13 #for 1/3 arc second data
 kind = IMG #for .img files
-location = n40w124
+location = 'n%sw%s' % (str(latitude), str(longitude))
 
-path = 'ftp://rockyftp.cr.usgs.gov/vdelivery/Datasets/Staged/NED/%s/%s/' % (resolution, kind)
+URL = 'ftp://rockyftp.cr.usgs.gov/vdelivery/Datasets/Staged/NED/%s/%s/' % (str(resolution), str(kind))
+
+
+page = urllib2.urlopen(URL)
+soup = BeautifulSoup(page, "lxml")
+div = str(soup.find('div', class_ = 'well'))
+log = open("log.txt", "w")
+log.write(div)
+log.close()
+f = open("log.txt")
+lines = f.readlines()
+line = lines[short_name_line]
+short_name = line[short_name_shear:]
+f.close()
+os.remove("log.txt")
+short_name = short_name.rstrip()
