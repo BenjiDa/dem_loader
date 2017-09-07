e1e28cd194962b5f1b76a359eabd813cc154912c
Benjamin L Melosh <bmelosh@usgs.gov>
2017-09-07 10:00:29 -0700

    working on downloads/file size

--- download_dem.py
+++ download_dem.py
@@ -12,35 +12,7 @@ import lxml
 # the user.
 
 
-def dem_loader(latitude, longitude, resolution=13, kind='IMG'):
-    '''
-    A function that downloads DEM data from the National Elevation Dataset. Currently it only works for 1/3 arcsecond data (~10m),
-    but this can be expanded upon.
 
-    User inputs:
-    latitude = latitude position of area of interest
-    longitude = longitude position of area of interest
-    resoultion = the data resolution (default 1/3 arcsecond resolution (~10 meter))
-    kind = the format (default is .IMG), this can be expanded upon in the future
-    '''
-
-    new_folder_name = 'dem_%s' % location  # Set name of new folder to put data into
-
-    URL = 'ftp://rockyftp.cr.usgs.gov/vdelivery/Datasets/Staged/NED/%s/%s/' % (
-        str(resolution), kind)  # Full URL to DEM
-    filenames = ['n39w122', 'USGS_NED_13_n39w122_IMG']
-
-    source_directory = os.getcwd()  # Get current directory
-    # Set path name to new directory
-    data_directory = os.path.join(source_directory, new_folder_name)
-
-    # Check and see if the local directory currently exists or not, if not
-    # create that directory
-    if os.path.exists(data_directory):
-        os.chdir(data_directory)
-    else:
-        os.makedirs(new_folder_name)
-        os.chdir(data_directory)
 
 
 def write_file_names(latlon, kind='.zip', resolution=13):
@@ -50,6 +22,8 @@ def write_file_names(latlon, kind='.zip', resolution=13):
     kind: file format for download only options are '.zip' or '.jpg'
     resolution: 13 stands for 1/3 arcsecond which is ~10 m
     '''
+    # Define the URL
+    URL = 'ftp://rockyftp.cr.usgs.gov/vdelivery/Datasets/Staged/NED/%s/IMG/' % str(resolution) 
 
     # Latitude and longitude are rounded up to position of the top
     # left corner of the DEM
@@ -59,21 +33,28 @@ def write_file_names(latlon, kind='.zip', resolution=13):
 
     # For loop that returns a list of file names corresponding to the input
     # latitude, longitude points.
-    location = []
+    filenames = []
     for x in range(0,len(latlon)):
-        loc = 'n%sw%s%s' % (str(latlon[x][0]), str(latlon[x][1]), kind)
-        location.append(loc)
+        loc = 'n%sw%s' % (str(latlon[x][0]), str(latlon[x][1]))
+        filenames.append(loc)
 
-    return location
+    return filenames
 
 
-def build_file_paths(filenames):
+def build_file_paths(filenames, kind='.zip', resolution=13):
+    # Create a dictionay of all the possible filename formats.
     all_filename_possibilities = []
-    for file in filenames:
-        # you can add as many odd filename builders here as you want
-        f = {'short_name': file + '.zip',
-             'long_name': 'USGS_NED_13_' + file + '_IMG.zip'}
-        all_filename_possibilities.append(f)
+    if kind == '.zip':
+        for file in filenames:
+            f = {'short_name': file + '.zip',
+                 'long_name': 'USGS_NED_13_' + file + '_IMG.zip' }
+            all_filename_possibilities.append(f)
+    else: 
+        for file in filenames:
+            f = {'short_name': 'img' + file + '_' + str(resolution) + '_thumb.jpg',
+                 'short_name_alternate': file + '_' + str(resolution) + '_thumb.jpg',
+                 'long_name': 'USGS_NED_13_' + file + '_IMG_thumb.jpg' }
+            all_filename_possibilities.append(f)
 
     return all_filename_possibilities
 
